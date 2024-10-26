cf9ed51d19bc0574ccab5c88249d063b9966a2dd
Antony <Antonyflores0609@gmail.com>
2024-10-26 11:36:51 -0400

    Js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hola mundo!")
+
+for( let numero= 1; numero <=20; numero++){
+    
+
+    if (numero % 2 === 0)
+        console.log(numero+" PAR")
+    else{
+        console.log(numero+" IMPAR")
+    }
+
+    
+    
+
+};
