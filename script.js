feb3fc540596ac14a795fa00a7357d48f622198c
Antony <Antonyflores0609@gmail.com>
2024-10-26 12:03:18 -0400

    Js02

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 console.log("Hola mundo!")
 
-for( let numero= 1; numero <=20; numero++){
-    
-
-    if (numero % 2 === 0)
-        console.log(numero+" PAR")
-    else{
-        console.log(numero+" IMPAR")
+function numerosParesImpares() {
+    for (let numero = 1; numero <= 20; numero++) {
+        if (numero % 2 === 0)
+            console.log(numero + " PAR")
+        else {
+            console.log(numero + " IMPAR")
+        }
     }
-
-    
-    
-
 };
+
